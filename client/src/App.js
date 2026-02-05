a6ea2695d5eee766464fb5c1f31a2406f5504e9a
amosmichiekasimiti-dotcom <amosmichiekasimiti@gmail.com>
2026-02-05 19:43:30 +0300

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Zion Trading Lab Live</h1>
+      <p>Analyzing Volatility 10 (1s) and Volatility 15...</p>
+    </div>
+  );
+}
+
+export default App;
